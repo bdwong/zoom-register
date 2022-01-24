b07aee60abd564b1b16f7e2f43e3e1d73d007acf
Brian D. Wong <bdwong.net@gmail.com>
2022-01-23 19:44:06 -0800

    Read registration list from csv file.

--- zoom-register.js
+++ zoom-register.js
@@ -1,2 +1,10 @@
+const neatCsv = require('neat-csv');
+
 // Read app configuration
 const appconfig = JSON.parse(fs.readFileSync('appconfig.json'));
+
+// Read meeting registrants
+function getRegistrants(filename) {
+   const csv = fs.readFileSync(filename, 'utf8');
+   return neatCsv(csv);
+ }
