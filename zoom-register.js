a68baeed7fa2f4d7f7459278cd2e964dc0ff3adf
Brian D. Wong <bdwong.net@gmail.com>
2022-10-27 14:56:00 -0700

    Move config functions into lib/config.js

--- zoom-register.js
+++ zoom-register.js
@@ -1,10 +1,10 @@
 const { Command, CommanderError, Argument } = require('commander');
-const { mkdirSync, readFileSync, copyFileSync } = require('fs');
+const { readFileSync } = require('fs');
 const neatCsv = require('neat-csv');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const { memoryUsage } = require('process');
-const path = require('path');
+const { appconfig } = require('./lib/config');
 
 // Read meeting registrants
 function getRegistrants(filename) {
@@ -59,45 +59,6 @@ async function in_chunks(array, chunk, func) {
       temporary = array.slice(i, i + chunk);
       await func(temporary);
    }
-}
-
-function config_dir() {
-   const path = require('path');
-   const os = require('os');
-   return path.join(os.homedir(), '.config', 'zoom-register');
-}
-
-function config_filepath() {
-   return path.join(config_dir(), 'appconfig.json');
-}
-
-function config_template_path() {
-   return path.join(__dirname, 'appconfig.sample.json');
-}
-
-function configure() {
-   mkdirSync(config_dir(), {recursive: true})
-   let src = config_template_path();
-   let dst = config_filepath();
-
-   console.log(`Copying template config to ${dst}`);
-   copyFileSync(src, dst);
-}
-
-// Read app configuration
-function readAppConfig() {
-   let config_data;
-   try {
-      config_data = JSON.parse(readFileSync(config_filepath()));
-   } catch(err) {
-      configure();
-   }
-   if (!config_data || config_data.api_key == 'YOUR_JWT_API_KEY_HERE' || config_data.api_secret == 'YOUR_JWT_SECRET_HERE') {
-      console.log(`Please edit ${config_filepath()} to include your Zoom API key and secret.`);
-      console.log('See https://github.com/bdwong/zoom-register#readme for more information.');
-      process.exit(2);
-   }
-   return config_data;
 }
 
 
@@ -210,8 +171,6 @@ attendees.command('delete')
          console.log('Deleted successfully.');
       }
    });
-
-const appconfig = readAppConfig();
 
 // Set request defaults
 axios.defaults.baseURL = appconfig['base_url'];
